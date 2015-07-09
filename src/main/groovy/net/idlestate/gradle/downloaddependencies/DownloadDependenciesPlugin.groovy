1f4d1c9e37fe20b3cb9067f1a1ac61b562e2a208
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2015-07-09 08:03:15 +0200

    Added automatic replacement of repositories with local repository

--- src/main/groovy/net/idlestate/gradle/downloaddependencies/DownloadDependenciesPlugin.groovy
+++ src/main/groovy/net/idlestate/gradle/downloaddependencies/DownloadDependenciesPlugin.groovy
@@ -7,9 +7,23 @@ import org.gradle.api.Project
  * Gradle-Plugin that creates a local maven repository with all dependencies. 
  */
 class DownloadDependenciesPlugin implements Plugin<Project> {
+    static final String DOWNLOAD_DEPENDENCIES_TASK = 'downloadDependencies'
 
     void apply( Project project ) {
-        project.task( 'downloadDependencies', type: DownloadDependenciesTask, group: 'Build Setup', description: 'Downloads all dependencies into a local directory based repository.' )
+        project.task( DOWNLOAD_DEPENDENCIES_TASK, type: DownloadDependenciesTask, group: 'Build Setup', description: 'Downloads all dependencies into a local directory based repository.' )
+
+        // Use only local repository, if download is not intended
+        project.gradle.taskGraph.whenReady { taskGraph ->
+            if ( !taskGraph.hasTask( ":${DOWNLOAD_DEPENDENCIES_TASK}" ) ) {
+                project.logger.info( "Replacing all defined repositories with local repository at 'gradle/repository'" )
+
+                project.repositories.clear()
+                project.repositories {
+                    maven {
+                        url 'gradle/repository'
+                    }
+                }
+            }
+        }
     }
 }
-
